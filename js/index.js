3bf15094b12e5b0cee829008c217f07d19cc193e
alexandre steinhauslin <a.steinhauslin@gmail.com>
2020-04-10 18:02:23 -0700

    Task 1 and 2 complete

--- js/index.js
+++ js/index.js
@@ -39,4 +39,66 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', 'img/logo.png');
+
+
+//Task 1
+let cta = document.querySelector("#cta-img");
+cta.src = 'img/header-img.png';
+
+let middleImage = document.querySelector("#middle-img");
+middleImage.src = 'img/mid-page-accent.jpg';
+
+let ctaText = document.querySelector(".cta-text");
+
+let navItems = document.querySelectorAll("a");
+navItems[0].textContent = "Services";
+navItems[1].textContent = "Product";
+navItems[2].textContent = "Vision";
+navItems[3].textContent = "Features";
+navItems[4].textContent = "About";
+navItems[5].textContent = "Contact";
+
+let mainTitle = document.querySelector("h1");
+mainTitle.textContent = "DOM IS AWESOME";
+
+let button = document.querySelector("button");
+button.textContent = "Get Started";
+
+//top
+let topTitles = document.querySelectorAll(".top-content h4");
+topTitles[0].textContent = "FEATURES";
+topTitles[1].textContent = "ABOUT";
+
+let topParagraphs = document.querySelectorAll(".top-content p");
+topParagraphs[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+topParagraphs[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+//bottom
+let bottomTitles = document.querySelectorAll(".bottom-content h4");
+bottomTitles[0].textContent = "SERVICES";
+bottomTitles[1].textContent = "PRODUCT";
+bottomTitles[2].textContent = "VISION";
+
+
+let bottomParagraphs = document.querySelectorAll(".bottom-content p");
+bottomParagraphs[0].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+bottomParagraphs[1].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+bottomParagraphs[2].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+let contactTitle = document.querySelector(".contact h4");
+contactTitle.textContent = "CONTACT";
+
+let contactParagraphs = document.querySelectorAll(".contact p");
+contactParagraphs[0].textContent = "123 Way 456 Street Somewhere, USA";
+//how do i <br> ??????????
+contactParagraphs[1].textContent = "1 (888) 888-8888";
+contactParagraphs[2].textContent = "sales@greatidea.io";
+
+
+let footerParagraph = document.querySelector("footer p");
+footerParagraph.textContent = "Copyright Great Idea! 2018";
+
+//Task 2
+
+
